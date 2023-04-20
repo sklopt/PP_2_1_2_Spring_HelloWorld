7fc64d46c139f38731e5ad80573ba509a1e3cee2
Shamil Gilmanov <sklopt@rambler.ru>
2023-04-20 19:20:49 +0500

    project completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,19 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Objects;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+                (HelloWorld) applicationContext.getBean("hello-world", HelloWorld.class);
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("hello-world", HelloWorld.class);
+        System.out.println(bean == bean2);
+        Cat beanCat = (Cat) applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat", Cat.class);
+        System.out.println(beanCat == beanCat2);
     }
 }
